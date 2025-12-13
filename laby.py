48f46f1e1a2608ad47c6c587476cfe7186644501
Melon22815 <ilianeogez@gmail.com>
2025-12-13 16:07:57 +0100

    Enhance maze generator with pathfinding and GUI updates
    Added neighbor tracking and pathfinding functionality to the maze generator. Updated the GUI to include a solve button and improved maze drawing.

--- laby.py
+++ laby.py
@@ -2,6 +2,8 @@
 import tkinter as Tk
 from random import randint
 from tkinter import font
+import time
+
 # Création d'une cellule.
 class Cellule():
     def __init__(self, x, y):
@@ -9,6 +11,7 @@ class Cellule():
         self.y = y
         self.visitee = False
         self.murs = {'N': True, 'E': True, 'S': True, 'W': True}
+        self.voisins = {'N': None, 'E': None, 'S': None, 'W': None}
 # Création de la grille du labyrinthe.
 class Grille():
     def __init__(self, l, c):
@@ -20,76 +23,131 @@ class Grille():
             for j in range(self.c):
                 grille_ligne.append(Cellule(j, i))
             self.cadrillage.append(grille_ligne)
-# Accès à une cellule donnée.
+    # Accès à une cellule donnée.
     def cellule(self, x, y):
-        if 0<=x<self.l and 0<=y<self.c:
-            return self.cadrillage[x][y]
-# Suppression des murs entre les cellules.
+        if 0<=x<self.c and 0<=y<self.l:
+            return self.cadrillage[y][x]
+    
+    def add_voisins(self):
+        for i in range(self.l):
+            for j in range(self.c):
+                if j < self.c-1:
+                    self.cellule(j, i).voisins['E'] = (j + 1, i)
+                if j > 0:
+                    self.cellule(j, i).voisins['W'] = (j - 1, i)
+                if i < self.l-1:
+                    self.cellule(j, i).voisins['S'] = (j, i + 1)
+                if i > 0:
+                    self.cellule(j, i).voisins['N'] = (j, i - 1)
+
+
+    # Suppression des murs entre les cellules.
     def effaceMur(self, orientation, coord):
         if orientation == 'N':
             self.cellule(coord[0],coord[1]).murs[orientation] = False
-            if 0 > coord[0]-1:
-                self.cellule(coord[0]-1, coord[1]).murs['S'] = False
+            if 0 <= coord[1]-1:
+                self.cellule(coord[0], coord[1]-1).murs['S'] = False
         if orientation == 'S':
             self.cellule(coord[0],coord[1]).murs[orientation] = False
-            if self.l > coord[0]+1:
-                self.cellule(coord[0]+1, coord[1]).murs['N'] = False
+            if self.l > coord[1]+1:
+                self.cellule(coord[0], coord[1]+1).murs['N'] = False
         if orientation == 'W':
             self.cellule(coord[0],coord[1]).murs[orientation] = False
-            if 0 < coord[1]-1:
-                self.cellule(coord[0], coord[1]-1).murs['E'] = False
+            if 0 <= coord[0]-1:
+                self.cellule(coord[0]-1, coord[1]).murs['E'] = False
         if orientation == 'E':
             self.cellule(coord[0],coord[1]).murs[orientation] = False
-            if self.c > coord[1]+1:
-                self.cellule(coord[0], coord[1]+1).murs['W'] = False
-# Affichage du labyrinthe.
+            if self.c > coord[0]+1:
+                self.cellule(coord[0]+1, coord[1]).murs['W'] = False
+    # Affichage du labyrinthe.
     def __str__(self):
         """
         Génère une représentation textuelle du labyrinthe.
         """
         laby_lignes = []
         laby_l = ['+']
-        for y in range(self.c):
-            if self.cadrillage[0][y].murs['N']:
+        for x in range(self.c):
+            if self.cadrillage[0][x].murs['N']:
                 laby_l.append('---+')
             else :
                 laby_l.append('   +')
         laby_lignes.append(''.join(laby_l)) 
-        for x in range(0,self.l):
-            if self.cadrillage[x][0].murs['W'] :
+        for y in range(0,self.l):
+            if self.cadrillage[y][0].murs['W'] :
                 laby_l = ['|']
             else :
                 laby_l = [' ']
-            for y in range(self.c):
-                if self.cadrillage[x][y].murs['E']:
+            for x in range(self.c):
+                if self.cadrillage[y][x].murs['E']:
                     laby_l.append('   |')
                 else:
                     laby_l.append('    ')
             laby_lignes.append(''.join(laby_l))
             laby_l = ['+']
-            for y in range(self.c):
-                if self.cadrillage[x][y].murs['S']:
+            for x in range(self.c):
+                if self.cadrillage[y][x].murs['S']:
                     laby_l.append('---+')
                 else:
                     laby_l.append('   +')
             laby_lignes.append(''.join(laby_l))
         #laby_lignes.append('\n')
         return '\n'.join(laby_lignes)
-# Génération du labyrinthe avec l'algorithme Sidewinder.
-def exploration_sidewinder(grille):
-    for i in range(grille.l):
-        deb_parcour = 0
-        for j in range(grille.c):
-            destroy = randint(0, 1)
-            if (destroy == 0 and j < grille.c-1) or (i == grille.l - 1 and j < grille.c-1):
-                grille.effaceMur('E', (i, j))
-            elif i < grille.l - 1:
-                k = randint(deb_parcour, j)
-                grille.effaceMur('S', (i, k))
-                deb_parcour = j+1
-    return grille
 
-labyrinthe = exploration_sidewinder(Grille(20, 20))
+solution = []
+
+#résolution du labyrinthe avec parcour dsf
+def dsf(laby, start, end):
+    pile = [start]
+    path = []
+    while pile:
+        s = pile.pop()
+        laby.cellule(s[0], s[1]).visitee = True
+        is_voisin = False
+        if s == end:
+            path.append(s)
+            return path
+        for t in laby.cellule(s[0], s[1]).voisins:
+            voisin = laby.cellule(s[0], s[1]).voisins[t]
+            if voisin != None and laby.cellule(s[0], s[1]).murs[t] == False and laby.cellule(voisin[0], voisin[1]).visitee == False:
+                is_voisin = True
+                pile.append(voisin)
+        
+        while not is_voisin and path:
+            s = path.pop()
+            for t in laby.cellule(s[0], s[1]).voisins:
+                voisin = laby.cellule(s[0], s[1]).voisins[t]
+                if voisin != None and laby.cellule(s[0], s[1]).murs[t] == False and laby.cellule(voisin[0], voisin[1]).visitee == False:
+                    is_voisin = True
+                    pile.append(voisin)
+        path.append(s)
+    return "Erreur"
+
+labyrinthe = Grille(20, 20)
+
+# Génération du labyrinthe avec l'algorithme Sidewinder.
+def exploration_sidewinder():
+    global labyrinthe
+    global solution
+    if solution:
+        dessiner_soluce(True)
+    labyrinthe = Grille(20, 20)
+    for i in range(labyrinthe.l):
+        deb_parcour = 0
+        for j in range(labyrinthe.c):
+            destroy = randint(0, 1)
+            if (destroy == 0 and j < labyrinthe.c-1) or (i == labyrinthe.l - 1 and j < labyrinthe.c-1):
+                labyrinthe.effaceMur('E', (j, i))
+            elif i < labyrinthe.l - 1:
+                k = randint(deb_parcour, j)
+                labyrinthe.effaceMur('S', (k, i))
+                deb_parcour = j+1
+    labyrinthe.add_voisins()
+    solution = dsf(labyrinthe, (0,0), (labyrinthe.c-1,0))
+    print(solution)
+    return labyrinthe
+
+
+
 
 # Création de la fenêtre.
 root = Tk.Tk()
@@ -147,15 +205,17 @@ menu_bar = Tk.Menu(root)
 file_menu = Tk.Menu(menu_bar, tearoff=0)
 file_menu.add_command(label="Quitter", command=close_window)
 file_menu.add_command(label="Relancer le programme", command=lambda: root.destroy() or __import__('laby'))
-file_menu.add_command(label="Générer un labyrinthe", command=lambda: dessiner_grillage(exploration_sidewinder(Grille(20, 20))))
+file_menu.add_command(label="Générer un labyrinthe", command=lambda: dessiner_grillage())
 menu_bar.add_cascade(label="Fichier", menu=file_menu)
 root.config(menu=menu_bar)
 # Le carré du labyrinthe.
 carre_labyrinthe = canvas.create_rectangle(100, 100, 500, 500, fill="#ffffff", outline="#333333", width=3)
 # Un bouton qui génène-ra le labyrinthe.
-bouton = Tk.Button(root, text="Générer le labyrinthe", command=lambda: dessiner_grillage(exploration_sidewinder(Grille(20, 20))))
-bouton.place(x=295, y=520, anchor="n")
-# Personnalisation du bouton.
+bouton = Tk.Button(root, text="Générer le labyrinthe", command=lambda: dessiner_grillage())
+bouton.place(x=195, y=520, anchor="n")
+bouton_soluce = Tk.Button(root, text="Résoudre le labyrinthe", command=lambda: dessiner_soluce())
+bouton_soluce.place(x=400, y=520, anchor="n")
+# Personnalisation des bouton.
 bouton.config(
     fg="#000000",
     bg="#f0f0f0",
@@ -171,17 +231,34 @@ bouton.config(
     highlightcolor="#ff6600",
     takefocus=True
 )
+bouton_soluce.config(
+    fg="#000000",
+    bg="#f0f0f0",
+    padx=10,
+    pady=5,
+    borderwidth=2,
+    relief="raised",
+    font=("Helvetica", 12, "bold", "italic"),
+    activebackground="#fffaf0",
+    activeforeground="#000000",
+    highlightthickness=2,
+    highlightbackground="#444444",
+    highlightcolor="#ff6600",
+    takefocus=True
+)
 # Le grillage du labyrinthe :
-def dessiner_grillage(grille):
+def dessiner_grillage():
     # fonction qui permet de tracer les murs du labyrinthe avec une marge de 99px et 
     # de longueur 20px en fonction du nombre de colonne et de lignes du labyrinthe
-    for i in range(0, grille.l * 20, 20):
-        for j in range(0, grille.c * 20, 20):
-            if grille.cellule(int(i/20), int(j/20)).murs['E']:
+    global labyrinthe
+    exploration_sidewinder()
+    for i in range(0, labyrinthe.l * 20, 20):
+        for j in range(0, labyrinthe.c * 20, 20):
+            if labyrinthe.cellule(int(j/20), int(i/20)).murs['E']:
                 canvas.create_line(j + 99 + 20, i + 99, j + 20 + 99, i + 20 + 99, fill="#000000")
             else:
                 canvas.create_line(j + 20 + 99, i + 99, j + 20 + 99, i + 20 + 99, fill="#FFFFFF")
-            if grille.cellule(int(i/20), int(j/20)).murs["S"]:
+            if labyrinthe.cellule(int(j/20), int(i/20)).murs["S"]:
                 canvas.create_line(j + 99, i + 20 + 99, j + 20 + 99, i + 20 + 99, fill="#000000")
             else:
                 canvas.create_line(j + 99, i + 20 + 99, j + 20 + 99, i + 20 + 99, fill="#FFFFFF")
@@ -191,7 +268,22 @@ def dessiner_grillage(grille):
     canvas.create_line(99, 499, 499, 499, fill="#ffffff")
     canvas.create_line(499, 99, 499, 499, fill="#ffffff")
 
-dessiner_grillage(labyrinthe)
+def dessiner_soluce(clean=False):
+    global solution
+
+    if clean:
+        for pos in solution:
+            canvas.create_rectangle(pos[0]*20+99+5, pos[1]*20+99+5,
+                                    pos[0]*20+99+15, pos[1]*20+99+15,
+                                    fill="#FFFFFF", outline="")
+    else:
+        for pos in solution:
+            canvas.create_rectangle(pos[0]*20+99+5, pos[1]*20+99+5,
+                                    pos[0]*20+99+15, pos[1]*20+99+15,
+                                    fill="#5FFF81", outline="")
+        
+
+dessiner_grillage()
 # Lancement de la boucle.
 root.mainloop()
 
